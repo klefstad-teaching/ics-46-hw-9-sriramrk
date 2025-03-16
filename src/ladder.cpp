4f05b8bb640dd3aa717bf77355ede22ac6ae8c28
Sriram Reddy Kudamala <skudamal@uci.edu>
2025-03-16 20:16:38 +0000

    wrote print and verify

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -82,3 +82,41 @@ bool edit_distance_within(const string& str1, const string& str2, int d)
     }
     return diff + (str_len1 - i) + (str_len2 - j) <= d;
 }
+
+void print_word_ladder(const vector<string>& ladder) 
+{
+    if (ladder.empty()) 
+    {
+        cout << "No ladder found." << endl;
+        return;
+    }
+    for (size_t i = 0; i < ladder.size(); ++i) 
+    {
+        cout << ladder[i];
+        if (i < ladder.size() - 1) cout << " -> ";
+    }
+    cout << endl;
+}
+
+#define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
+
+void verify_word_ladder() {
+
+    set<string> word_list;
+
+    load_words(word_list, "words.txt");
+
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+
+}
+
